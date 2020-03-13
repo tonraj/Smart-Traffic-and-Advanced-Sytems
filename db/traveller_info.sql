080525dbbe4c305c92ebc0cc7012df41ce13bca0
tonraj <rs188282@gmail.com>
2020-03-13 15:54:40 +0530

    Create traveller_info.sql

---
+++ db/traveller_info.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[traveller]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [engine_id] NCHAR(75) NOT NULL, 
+    [receiver_id] NCHAR(75) NULL, 
+    [sensor_id] NCHAR(75) NULL, 
+    [time] TIMESTAMP NULL
+)
